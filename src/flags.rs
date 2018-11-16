726ea8f4acf3c317a5b23c844e863abd08e6639d
Caio Souza <caiophilipevs@gmail.com>
2018-11-16 01:07:47 -0200

    Flags.
    Added function to set the Zero, Negative, Carry and Overflow based on addition.
    
    Better tests.

--- src/flags.rs
+++ src/flags.rs
@@ -28,6 +28,19 @@ impl Flags {
             | (((value == 0) as u8) * Self::Zero.bits)
             | (value & (Self::Negative.bits | Self::Overflow.bits));
     }
+
+    // Set the Zero, Negative, Carry and Overflow flags according to the sum of the values passed
+    pub fn znco(&mut self, a: u8, b: u8) {
+        let value: u16 = a as u16 + b as u16;
+
+        self.bits = (self.bits & !Self::Zero.bits & !Self::Negative.bits & !Self::Carry.bits & !Self::Overflow.bits)
+            | ((((value as u8) == 0) as u8) * Self::Zero.bits)
+            | ((value as u8) & Self::Negative.bits)
+            | (((value & 0x0100u16) >> 8) as u8) // Carry happens if bit 8 is set
+            // Overflow happens when the sign of the addends is the same and differs from the sign of the sum
+            | ((!(a ^ b) & (a ^ value as u8) & 0x80) >> 1)
+        ;
+    }
 }
 
 impl From<u8> for Flags {
@@ -38,64 +51,71 @@ impl From<u8> for Flags {
 mod zn {
     use super::*;
 
-    #[test]
-    fn zero() {
-        let mut flags = Flags::empty();
-        flags.zn(0);
-        assert_eq!(flags.bits(), 0b00000010);
+    fn test(value: u8, mut initial: Flags, result: Flags) {
+        initial.zn(value);
+        assert_eq!(initial, result, "\nvalue: {:x?}", value);
     }
 
     #[test]
-    fn positive() {
-        let mut flags = Flags::all();
-        flags.zn(127);
-        assert_eq!(flags.bits(), 0b01111101);
-    }
+    fn zero() { test(0, Flags::empty(), Flags::Zero); }
 
     #[test]
-    fn negative() {
-        let mut flags = Flags::empty();
-        flags.zn(128);
-        assert_eq!(flags.bits(), 0b10000000);
-    }
+    fn positive() { test(127, Flags::all(), !Flags::Negative & !Flags::Zero); }
+
+    #[test]
+    fn negative() { test(128, Flags::empty(), Flags::Negative); }
 }
 
 #[cfg(test)]
 mod zno_bit_test {
     use super::*;
 
+    fn test(value: u8, mut initial: Flags, result: Flags) {
+        initial.zno_bit_test(value);
+        assert_eq!(initial, result, "\nvalue: {:x?}", value);
+    }
+
     #[test]
-    fn zero() {
+    fn zero() { test(0, Flags::empty(), Flags::Zero); }
+
+    #[test]
+    fn positive() { test(63, Flags::all(), !Flags::Negative & !Flags::Overflow & !Flags::Zero); }
+
+    #[test]
+    fn positive_overflow() { test(127, Flags::all(), !Flags::Negative & !Flags::Zero); }
+
+    #[test]
+    fn negative() { test(128, Flags::empty(), Flags::Negative); }
+
+    #[test]
+    fn negative_overflow() { test(204, Flags::empty(), Flags::Negative | Flags::Overflow); }
+}
+
+#[cfg(test)]
+mod znco {
+    use super::*;
+
+    fn test(a: u8, b: u8, result: Flags) {
         let mut flags = Flags::empty();
-        flags.zno_bit_test(0);
-        assert_eq!(flags.bits(), 0b00000010);
+        flags.znco(a, b);
+        assert_eq!(flags, result, "\nvalue: {:x?}", a as u16 + b as u16);
     }
 
     #[test]
-    fn positive() {
-        let mut flags = Flags::all();
-        flags.zno_bit_test(63);
-        assert_eq!(flags.bits(), 0b00111101);
-    }
+    fn sum_0x00_0x00() { test(0x00u8, 0x00u8, Flags::Zero); }
 
     #[test]
-    fn positive_overflow() {
-        let mut flags = Flags::all();
-        flags.zno_bit_test(127);
-        assert_eq!(flags.bits(), 0b01111101);
-    }
+    fn sum_0xa0_0xa0() { test(0xa0u8, 0xa0u8, Flags::Carry | Flags::Overflow); }
 
     #[test]
-    fn negative() {
-        let mut flags = Flags::empty();
-        flags.zno_bit_test(128);
-        assert_eq!(flags.bits(), 0b10000000);
-    }
+    fn sum_0x90_0x90() { test(0x90u8, 0x90u8, Flags::Carry | Flags::Overflow); }
 
     #[test]
-    fn negative_overflow() {
-        let mut flags = Flags::empty();
-        flags.zno_bit_test(204);
-        assert_eq!(flags.bits(), 0b11000000);
-    }
+    fn sum_0x80_0x80() { test(0x80u8, 0x80u8, Flags::Zero | Flags::Carry | Flags::Overflow); }
+
+    #[test]
+    fn sum_0x00_0x80() { test(0x00u8, 0x80u8, Flags::Negative); }
+
+    #[test]
+    fn sum_0xe0_0xe0() { test(0xe0u8, 0xe0u8, Flags::Negative | Flags::Carry); }
 }
